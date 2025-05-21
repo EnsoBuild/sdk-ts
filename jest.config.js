3753599d63ac166d5c290df1c7a8e911b6de89db
Nenad Vitorovic <nvitorovic@gmail.com>
2025-05-21 19:12:43 +0200

    skip coverage

--- jest.config.js
+++ jest.config.js
@@ -11,12 +11,4 @@ export default{
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov", "clover"],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
 };
