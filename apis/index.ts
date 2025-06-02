99ac211ada482928fabf13096d27fe59c06cbfe2
Smrecz <diegoxlt@gmail.com>
2025-06-02 10:56:47 +0200

    generated sdk test

---
+++ apis/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './MetadataApi';
+export * from './ProjectsApi';
+export * from './ShortcutsApi';
+export * from './StandardsApi';
+export * from './WalletApi';
